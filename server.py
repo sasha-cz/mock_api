8a73690ff63c916b134ce1486551c5a71c4577e6
sasha_cz <alexandra.pietroch@gmail.com>
2023-12-04 18:17:04 +0100

    fix TypError in server.py

--- server.py
+++ server.py
@@ -20,15 +20,9 @@ class MyServer(BaseHTTPRequestHandler):
 # Send the HTTP response body to the client
             self.wfile.write(response)
 
-# When the script is executed directly, we want to get hold of the port number of the hosting environment (e.g., PaaS like Heroku). 
-# the port variable is set to the value of the 'PORT' environment variable if it exists. 
-# If the 'PORT' environment variable is not set, it defaults to 3000.
-# Additionaly, a server_adress gets configured, so the server will listen to all available network interfaces and the retrieved port number.
+# Create a Server, that listens for incoming HTTP requests continuously. 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000)) 
-    server_adress = ("", port)
-
-# Create an HTTP server, print a message indicating that the server is starting, and then start the server to listen for incoming HTTP requests continuously.
-    with HTTPServer(server_adress, MyServer) as httpd:
-        print(f"Starting server on port {port}")
-        httpd.serve_forever()
+    serverPort = int(os.environ.get("PORT", 3000)) 
+    webServer = HTTPServer(("", serverPort), MyServer)
+    print(f"Starting server on port {serverPort}")
+    webServer.serve_forever()
