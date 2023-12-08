fe3e11529a75a5c2766c951ec4735af6be39df6b
sasha_cz <alexandra.pietroch@gmail.com>
2023-12-08 12:26:39 +0100

    Update comments in server.py

--- server.py
+++ server.py
@@ -7,19 +7,20 @@ app = Flask(__name__)
 # Import the 'mock_api' data from the 'db' module representing your database.
 from db import mock_api
 
-# Store an API key in an environment variable and fetch the API key from the operating system
+# Retrieve the API key from the environment variable.
 API_KEY = os.environ.get("API_KEY")
 
-# Use the decorator function 'route' from the Flask app object to define a route for the '/mock-api/books' URL
+# Use the decorator function 'route' from the Flask app object to set the root path and implement API key authentication. 
 @app.route("/", methods=["GET"])
 def index():
     api_key = request.args.get("api_key")
     if request.method=="GET":
         if api_key is None or api_key != API_KEY:
             return jsonify({"error": "Invalid API key"}), 401
-# Serialize mock_api to json format and create a JSON response
+# Serialize mock_api to json format and create a JSON response.
         return jsonify(mock_api)
+    
 # Run the Flask development server on all available network interfaces with debugging turned off,
-# suitable for deployment on platforms like Render.com
+# suitable for deployment on platforms like Render.com.
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 3000))) 
